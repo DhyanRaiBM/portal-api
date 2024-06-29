1c9f7387e643beb86ee87851965e0adb3246d7a8
Dhyan Rai <dhyanraibm@gmail.com>
2024-06-29 22:00:40 +0530

    Fnal commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
+// src/app.js
 import express from 'express';
 import cors from 'cors';
 import cookieParser from "cookie-parser";
+import serverless from 'serverless-http';
 
 const app = express();
 
@@ -8,9 +10,8 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN
 }));
 
-
-app.use(express.json({ limit: "16kb" })); //Json data limit
-app.use(express.urlencoded({ extended: true, limit: "16kb" })); //Accept form or any urlencoded data from request body and add it as req.body
+app.use(express.json({ limit: "16kb" })); // Json data limit
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // Accept form or any urlencoded data from request body and add it as req.body
 app.use(express.static("public"));
 app.use(cookieParser());
 
@@ -26,5 +27,13 @@ app.use("/api/listings", listingRouter);
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 app.use(errorMiddleware);
 
+// Create a new router for the root route
+const router = express.Router();
+router.get("/", (req, res) => {
+    res.send("App is running..");
+});
+app.use("/.netlify/functions/app", router);
 
+// Export app and handler
+export const handler = serverless(app);
 export default app;
